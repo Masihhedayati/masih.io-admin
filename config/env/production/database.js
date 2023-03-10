867a50b9ebe42e7bf08428a034dbffa53cf8ce63
Omar Faruq <naimislam3545@gmail.com>
2023-03-10 15:59:57 +0600

    chore: ready to deploy vm

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,6 +7,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
